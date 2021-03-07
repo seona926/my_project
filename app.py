678ce8c2865784758577ce7fe9473c528fdfd3f0
Sophia <suna9267@gmail.com>
2021-03-07 17:15:36 +0900

    modify add id/pw to mongoclient

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@localhost',27017)
 db = client.flags
 
 ## HTML을 주는 부분
@@ -18,6 +18,8 @@ def flag_get():
     result = list(db.flags.find({'countryName': country_receive}, {'_id': False}))
     return jsonify({'result': 'success', 'flags_list': result})
 
+# @app.route('/info', methods=['GET'])
+# def send_country_info():
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
